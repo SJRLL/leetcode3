6d76ac2058244dcb52b9f813f82d29d713af2354
SJRLL <1124335040@qq.com>
2020-05-23 21:47:56 +0800

    leetcode-98-验证二叉搜索树

---
+++ leetcode-98-验证二叉搜索树/Test.cpp
@@ -0,0 +1,39 @@
+﻿给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+示例 1 :
+
+输入 :
+	2
+	/ \
+	1   3
+输出: true
+示例 2 :
+
+ 输入 :
+	5
+	/ \
+	1   4
+	/ \
+	3   6
+输出: false
+解释 : 输入为 : [5, 1, 4, null, null, 3, 6]。
+	 根节点的值为 5 ，但是其右子节点值为 4 。
+
+//解题思路：因为二叉搜索树中序遍历是递增的, 所以我们可以中序遍历判断前一数是否小于后一个数.
+
+class Solution :
+	   def isValidBST(self, root: TreeNode) -> bool :
+	   res = []
+	   def helper(root) :
+   if not root :
+   return
+	   helper(root.left)
+	   res.append(root.val)
+	   helper(root.right)
+	   helper(root)
+	   return res == sorted(res) and len(set(res)) == len(res)
